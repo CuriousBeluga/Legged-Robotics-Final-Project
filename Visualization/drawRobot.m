eebc61a27031ff721aaebfd127283bbcaef0c0ba
Lucas Vanslette <lvanslette2@gmail.com>
2024-04-15 18:58:50 -0400

    updated visualization and added main.m

--- Visualization/drawRobot.m
+++ Visualization/drawRobot.m
@@ -1,15 +1,9 @@
 function drawRobot( ee, robot_parameters)
     % draw upper platform
-    create_circle( ee(1), ee(2), robot_parameters.upper/2, robot_parameters.l3i, ee, 'black' );
+    create_circle( ee(1), ee(2), robot_parameters.upper/2, ee(3), ee, 'black' );
     hold on
-    % create cone shape at the top
-    create_cone( ee, robot_parameters );
     % draw lower platform
     create_circle( 0, 0, robot_parameters.lower/2, 0, zeros([6,1]), 'black' );
-
-    xlim([-robot_parameters.lower robot_parameters.lower])
-    ylim([-robot_parameters.lower robot_parameters.lower])
-    zlim([0 robot_parameters.lower])
 
     % calculate direct (virtual) leg lengths
     [L_o, l, u_o, s_c, R_co] = inv_kin( ee, robot_parameters );
@@ -20,38 +14,6 @@ function drawRobot( ee, robot_parameters)
         z = [u_o(3,ii); u_o(3,ii)+L_o(3,ii)];
         plot3(x,y,z, 'Color', 'black'); hold on
     end
-    hold off
-end
-
-function create_cone( ee, robot_parameters )
-    % EVERYTHING WORKS UP UNTIL HERE
-    
-    r = robot_parameters.upper/2;
-    h = robot_parameters.upper/2;
-    m = (h/r);
-    [R,A] = meshgrid(linspace(0,r,11),linspace(0,2*pi,41));
-    X = (R .* cos(A));
-    Y = (R .* sin(A));
-    Z = (-m*R);
-    h=surf(X+ee(1),Y+ee(2),Z+ee(3)+robot_parameters.upper/2);
-    inclined_x = rad2deg(ee(4)); % in degrees
-    inclined_y = rad2deg(ee(5)); % in degrees
-    inclined_z = rad2deg(ee(6)); % in degrees
-    rotate(h, [1 0 0], inclined_x);
-    rotate(h, [0 1 0], inclined_y);
-    rotate(h, [0 0 1], inclined_z);
-% 
-%     [X,Y,Z]=cylinder([robot_parameters.upper/2 0]);
-%     
-%     M=makehgtform('translate',ee(1:3)); %  ,'xrotate',ee(4),'yrotate',pi+ee(5),'zrotate',ee(6));
-%     
-%     Z = Z*robot_parameters.upper/2;
-%     scatter3(ee(1), ee(2), ee(3), 10, 'filled');
-%     h=surf(X,Y,Z,'Parent',hgtransform('Matrix',M),'LineStyle','none','FaceAlpha',0.6);
-%     light
-%     zlim([0 400]);
-
-
 end
 
 function create_circle(xoffset,yoffset,r,platform_height,ee,color)
---
+++ main.m
@@ -0,0 +1,31 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% RBE 521 Final Project     %
+% Authors: Martin Bleakley  %
+%          Andrew Meyer     %
+%          Sean Tseng       %
+%          Lucas Vanslette  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% NOTE: the robot parameters are assumed to be known
+
+%% STL image processing code
+% inputs: stl file
+% outputs: end effector positions over time
+load('simple_stadium_data.mat');
+
+%% Inverse kinematics code
+% inputs: end effector positions over time
+% outputs: leg lengths over time
+
+
+%% Forward kinematics code
+% inputs: end effector positions over time
+% outputs: kinematic calibration
+
+
+%% Visualization code
+% inputs: leg lengths and end effector positions over time
+% outputs: visualized robot 
+plotTraj( ee_points, robot_parameters );
+
+
